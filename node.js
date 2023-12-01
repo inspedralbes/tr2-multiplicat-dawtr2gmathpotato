99ebd05691f863d7ee26d1c57a9ea29755ac5c45
Fabianroldan <a21fabrolfer@inspedralbes.cat>
2023-12-01 12:33:54 +0100

    FUNCIONAA!!!!!!1!!!!11!!

--- node.js
+++ node.js
@@ -4,7 +4,41 @@ const server = require("http").Server(app);
 const io = require('socket.io')(server);
 const fs = require('fs');
 const { Socket } = require('socket.io');
+var mysql = require('mysql');
 var usersConectados = [];
+
+const objPreguntes = {};
+
+//--------------------------BASE DE DATOS----------------------------
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "Potato"
+});
+
+con.connect(function(err){
+    if (err) throw err;
+    con.query("SELECT * FROM preguntas", function (err, pregunta) {
+        if (err) throw err;
+
+        for (let i = 0; i < pregunta.length; i++) {
+
+            console.log("La pregunta es: ", pregunta[i].pregunta);           
+            const resultatPregunta = eval(pregunta[i].pregunta);
+            console.log("--> ", resultatPregunta);
+            
+            objPreguntes[i] = {
+                id: pregunta[i].id_pregunta,
+                pregunta: pregunta[i].pregunta,
+            };
+        }
+     
+    });
+});
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -41,7 +75,12 @@ io.on('connection', (socket) => {
             console.error("Error ", error);
         }
     });
+    
     socket.emit("username");
+
+    socket.emit('preguntas', objPreguntes);
+        
+    console.log('Objeto con las preguntas y sus resultados:', objPreguntes);
 });
 
 server.listen(3000, () => {
