bf9cfd60efb461e66739670a66b93882be1620aa
Fabianroldan <a21fabrolfer@inspedralbes.cat>
2023-12-01 12:17:27 +0100

    Revert "Merge branch 'develop' into base-datos-back"
    This reverts commit 77cefdaa2f733bcc29ead3cae96b39b006e4a8d6, reversing
    changes made to a79e1a4dc3a5b112eefc485fd60890eb76517cec.

--- node.js
+++ node.js
@@ -1,48 +1,27 @@
-import express from 'express';
-import cors from 'cors';
-import { createServer } from 'node:http';
-import { Server } from 'socket.io';
+const express = require('express');
 const app = express();
-
-app.use(cors());
-
+const server = require("http").Server(app);
+const io = require('socket.io')(server);
+const fs = require('fs');
+const { Socket } = require('socket.io');
 var usersConectados = [];
-const server = createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST'],
-    },
-});
+
 app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, '../../../index.html'));
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+    socket.on('Nuevo usuario', (nuevoUsuario) => {
         console.log("User connected.");
         console.log(socket.id);
-        if (usersConectados.length > 0) {
-            io.emit('nuevosUsuario', usersConectados);
-            console.log("hi");
-        }
-
+        
         try {
-
-            socket.on('join', (data) => {
-                usersConectados.push({username:data, id:socket.id});
-                console.log(data);
-                io.emit('nuevosUsuario', usersConectados);
-            });
-
-
-
-            // usersConectados.push(nuevoUsuario);
+            usersConectados.push(nuevoUsuario);
             
-            // socket.broadcast.emit('usuarioConectado', usersConectados);   
-            // for (let i = 0; i < usersConectados.length; i++) {
-            //     console.log("hola", usersConectados[i]);
-                
-            // }
+            socket.broadcast.emit('usuarioConectado', usersConectados);   
+            for (let i = 0; i < usersConectados.length; i++) {
+                console.log("hola", usersConectados[i]);
+            }
 
             socket.on('disconnect', () => {
                 const usuarioDesconectadoIndex = usersConectados.findIndex(user => user.id === socket.id);
@@ -56,16 +35,15 @@ io.on('connection', (socket) => {
                     io.emit('arrayUsers', usersConectados);
 
                 }
-                console.log('User Disconnected');
             });
 
         } catch (error) {
             console.error("Error ", error);
         }
-
+    });
     socket.emit("username");
 });
 
-server.listen(5175, () => {
-    console.log('Listening on http://localhost:5175');
+server.listen(3000, () => {
+    console.log('Listening on http://localhost:3000');
 });
