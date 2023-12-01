b9d974d27948e5318cb96f0d2d103b7fefe9c1f1
Fabianroldan <a21fabrolfer@inspedralbes.cat>
2023-12-01 12:18:01 +0100

    revert merge branch in to develop

--- node.js
+++ node.js
@@ -1,48 +1,60 @@
-import express from 'express';
-import cors from 'cors';
-import { createServer } from 'node:http';
-import { Server } from 'socket.io';
+const express = require('express');
 const app = express();
-
-app.use(cors());
-
+const server = require("http").Server(app);
+const io = require('socket.io')(server);
+const fs = require('fs');
+const { Socket } = require('socket.io');
+var mysql = require('mysql');
 var usersConectados = [];
-const server = createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST'],
-    },
+
+const objPreguntes = {};
+
+//--------------------------BASE DE DATOS----------------------------
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "Potato"
 });
+
+con.connect(function(err){
+    if (err) throw err;
+    con.query("SELECT * FROM preguntas", function (err, pregunta) {
+        if (err) throw err;
+
+        for (let i = 0; i < pregunta.length; i++) {
+
+            console.log("La pregunta es: ", pregunta[i].pregunta);           
+            const resultatPregunta = eval(pregunta[i].pregunta);
+            console.log("--> ", resultatPregunta);
+            
+            objPreguntes[i] = {
+                id: pregunta[i].id_pregunta,
+                pregunta: pregunta[i].pregunta,
+            };
+        }
+     
+    });
+});
+
+
+
 app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, '../../../index.html'));
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+    socket.on('Nuevo usuario', (nuevoUsuario) => {
         console.log("User connected.");
         console.log(socket.id);
-        if (usersConectados.length > 0) {
-            io.emit('nuevosUsuario', usersConectados);
-            console.log("hi");
-        }
-
+        
         try {
-
-            socket.on('join', (data) => {
-                usersConectados.push({username:data, id:socket.id});
-                console.log(data);
-                io.emit('nuevosUsuario', usersConectados);
-            });
-
-
-
-            // usersConectados.push(nuevoUsuario);
+            usersConectados.push(nuevoUsuario);
             
-            // socket.broadcast.emit('usuarioConectado', usersConectados);   
-            // for (let i = 0; i < usersConectados.length; i++) {
-            //     console.log("hola", usersConectados[i]);
-                
-            // }
+            socket.broadcast.emit('usuarioConectado', usersConectados);   
+            for (let i = 0; i < usersConectados.length; i++) {
+                console.log("hola", usersConectados[i]);
+            }
 
             socket.on('disconnect', () => {
                 const usuarioDesconectadoIndex = usersConectados.findIndex(user => user.id === socket.id);
@@ -56,16 +68,20 @@ io.on('connection', (socket) => {
                     io.emit('arrayUsers', usersConectados);
 
                 }
-                console.log('User Disconnected');
             });
 
         } catch (error) {
             console.error("Error ", error);
         }
-
+    });
+    
     socket.emit("username");
+
+    socket.emit('preguntas', objPreguntes);
+        
+    console.log('Objeto con las preguntas y sus resultados:', objPreguntes);
 });
 
-server.listen(5175, () => {
-    console.log('Listening on http://localhost:5175');
+server.listen(3000, () => {
+    console.log('Listening on http://localhost:3000');
 });
