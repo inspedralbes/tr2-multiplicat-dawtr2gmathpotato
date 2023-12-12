7adf622c937dd7d12889eed61db507f720147aab
a20aitbaresc <a20aitbaresc@inspedralbes.cat>
2023-12-12 13:25:09 +0100

    prueba

--- Back/api-laravel/app/Http/Controllers/UsuarisController.php
+++ Back/api-laravel/app/Http/Controllers/UsuarisController.php
@@ -1,35 +1,39 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
-use App\Models\Usuarios;
+use App\Models\User; // Asegúrate de importar correctamente el modelo User
 use Illuminate\Support\Facades\Hash;
 
 class UsuarisController extends Controller
 {
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         $request->validate([
             'username' => 'required|string|max:50',
             'email' => 'required|string|email|unique:usuaris',
             'password' => 'required|string|min:6|confirmed',
             'foto_perfil' => [
                 'required',
-                Rule::in(['1', '2', '3']), // Reemplaza 'valor1', 'valor2', 'valor3' con los valores permitidos
+                Rule::in(['1', '2', '3']),
             ],
         ]);
-        $usuari= new Usuarios();
-        $usuari->username=$request -> username;
-        $usuari->email =$request -> email;
-        $usuari->password=Hash::make($request -> password);
-        $usuari->foto_perfil=$request -> foto_perfil;
+
+        $usuari = new User(); // Asegúrate de utilizar el modelo User
+        $usuari->username = $request->username;
+        $usuari->email = $request->email;
+        $usuari->password = Hash::make($request->password);
+        $usuari->foto_perfil = $request->foto_perfil;
         $usuari->save();
+
         return response()->json([
             'status' => 1,
             'message' => 'Usuari creat correctament'
         ]);
-        
     }
+
     public function login(Request $request){
         $request->validate([
             'email' => 'required|string|email',
