21b9f933057e4d7ef13d877810ed7a6b56ea26a0
rublormar <a21rublormar@inspedralbes.cat>
2023-12-19 23:59:34 +0100

    changes complete

--- Back/api-laravel/app/Http/Controllers/UsuariosController.php
+++ Back/api-laravel/app/Http/Controllers/UsuariosController.php
@@ -10,25 +10,19 @@ use Illuminate\Support\Facades\Hash;
 class usuariosController extends Controller
 {
     public function register(Request $request) {
-<<<<<<< HEAD
-        
-            
-=======
-
->>>>>>> Register&LoginWorking
             $request->validate([
-                'nombre_usuario' => 'required|string|max:50',
+                'username' => 'required|string|max:50',
                 'email' => 'required|string|email|unique:usuarios',
-                'contraseña' => 'required|string|min:6|confirmed',
+                'password' => 'required|string|min:6|confirmed',
                 'foto_perfil' => [
                     'required',
                     Rule::in(['1', '2', '3', '4', '5', '6', '7', '8', '9']),
                 ],
             ]);
             $usuario = new Usuarios();
-            $usuario->nombre_usuario = $request->nombre_usuario;
+            $usuario->username = $request->username;
             $usuario->email = $request->email;
-            $usuario->contraseña = Hash::make($request->contraseña);
+            $usuario->password = Hash::make($request->password);
             $usuario->foto_perfil = $request->foto_perfil;
            
             $usuario->save();
@@ -42,19 +36,16 @@ class usuariosController extends Controller
     public function login(Request $request){
         $request->validate([
             'email' => 'required|string|email',
-            'contraseña' => 'required|string|min:6',
+            'password' => 'required|string|min:6',
         ]);
     
         // Cambia la línea siguiente para asignar el resultado de la consulta a $usuario
         $usuario = Usuarios::where("email", "=", $request->email)->first();
     
         if ($usuario) {
-            if (Hash::check($request->contraseña, $usuario->contraseña)) {
-                $token = $usuario->createToken('auth_token')->plainTextToken;
+            if (Hash::check($request->password, $usuario->password)) {
                 return response()->json([
-                    'status' => 1,
-                    'message' => 'usuario logejat correctament',
-                    'token' => $token
+                    
                 ]);
             } else {
                 return response()->json([
