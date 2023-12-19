0ddab5e4621e04ef52c4cbac3f06df9254f9e083
Fabianroldan <a21fabrolfer@inspedralbes.cat>
2023-12-19 21:47:27 +0100

    Revert "Merge branch 'Register&LoginWorking' into develop"
    This reverts commit 3492b5d0d6e64f7b8a68dbba259d9cb8b1ced5da, reversing
    changes made to a8303306af384880229a3c231abb26444e13bea8.

--- node.js
+++ node.js
@@ -56,76 +56,18 @@ io.on('connection', (socket) => {
             }
         }
 
-
-    });
-    socket.on('register', (userData) => {
-        console.log(userData);
-        fetch('http://localhost:8000/api/register', {
-            method: 'POST',
-            body: JSON.stringify(userData),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(response => {
-            if (response.ok) {
-                if (usersConectados.length === 0) {
-                    usersConectados.push({ username: userData.username, id: socket.id, bomba: true, image: './src/assets/Icon_' + userData.foto_perfil + '.png' });
-                } else {
-                    usersConectados.push({ username: userData.username, id: socket.id, bomba: false, image: './src/assets/Icon_' + userData.foto_perfil + '.png' });
-                }
-                io.emit('usersConnected', usersConectados);
-                return response.json();
-            } else {
-                console.log(response);
-                throw new Error('Network response was not ok.');
-
-            }
-        }).then(userData => {
-            console.log("aaaaaaaaaaaaaaaaaaaaa");
-            console.log(userData);
-        }).catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
-    });
-    socket.on('login', async (data) => {
-        console.log(data);
-        await getUser(data);
-    });
-    async function getUser(data) {
-        try {
-            console.log("data to send...", data)
-            const response = await fetch('http://localhost:8000/api/login', {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-            // console.log("response..??", response);
-            if (response) {
-
-                const responseData = await response.json();
-                console.log("response.ok....", responseData);
-
-                if (usersConectados.length === 0) {
-                    usersConectados.push({ username: responseData.username, id: socket.id, bomba: true, image: './src/assets/Icon_' + responseData.foto_perfil + '.png' });
-                } else {
-                    usersConectados.push({ username: responseData.username, id: socket.id, bomba: true, image: './src/assets/Icon_' + responseData.foto_perfil + '.png' });
-                }
-                io.emit('usersConnected', usersConectados);
-                return responseData;
-            } else {
-                console.log(response);
-                throw new Error('Network response was not ok.');
-            }
-        } catch (error) {
-            console.error('There has been a problem with your fetch operation:', error);
+        if (gameRooms[gameRooms.length - 1].users.length == 0) {
+            // Si no hay usuarios conectados, se agrega el primer usuario a la sala
+            gameRooms[gameRooms.length - 1].users.push({ username: data, id: socket.id, bomba: true, image: './src/assets/Icon_1.png', roomPosition: lastRoom });
+        } else {
+            // Si ya hay usuarios, se agrega un nuevo usuario a la sala
+            gameRooms[gameRooms.length - 1].users.push({ username: data, id: socket.id, bomba: false, image: './src/assets/Icon_1.png', roomPosition: lastRoom });
         }
         socket.join("gameRoom" + lastRoom);
         console.log(gameRooms[gameRooms.length - 1].users);
         io.to("gameRoom" + lastRoom).emit('usersConnected', gameRooms[gameRooms.length - 1].users, gameRooms[gameRooms.length - 1].roomName);
         console.log('Salas: ', io.sockets.adapter.rooms);
-    }
+    });
 
 
     socket.on('startGame', (data) => {
@@ -153,11 +95,11 @@ io.on('connection', (socket) => {
                 let preguntas = data;
                 console.log(preguntas.preguntas);
                 room.preguntas = preguntas.preguntas;
-
+      
             }).then(() => {
 
 
-                newPregunta(room);
+                    newPregunta(room);
             }
             );
     }
@@ -210,32 +152,32 @@ io.on('connection', (socket) => {
         gameRooms.forEach(room => {
             let usuarioDesconectadoIndex = room.users.findIndex(user => user.id === socket.id);
             console.log(usuarioDesconectadoIndex);
-            if (usuarioDesconectadoIndex !== -1) {
-                if (room.users[usuarioDesconectadoIndex].bomba) {
-                    if (usuarioDesconectadoIndex == room.users.length - 1) {
-                        room.users[0].bomba = true;
-                    } else {
-                        room.users[usuarioDesconectadoIndex + 1].bomba = true;
+            if (usuarioDesconectadoIndex !== -1){
+                if(room.users[usuarioDesconectadoIndex].bomba){
+                    if(usuarioDesconectadoIndex==room.users.length-1){
+                        room.users[0].bomba=true;
+                    } else{
+                        room.users[usuarioDesconectadoIndex+1].bomba=true;
                     }
                 }
                 let usuarioDesconectado = room.users.splice(usuarioDesconectadoIndex, 1);
                 socket.leave(room.roomName);
                 io.to(room.roomName).emit('usersDesconectados', room.users, room.roomName);
                 console.log('Usuario desconectado: ', usuarioDesconectado);
-
+            
             }
         });
 
 
 
     });
-    socket.on('login', (data) => {
+    socket.on('login', (data) => { 
         console.log(data);
     });
 
-});
-// socket.emit("username");
+    // socket.emit("username");
 
+});
 // io.emit('arrayUsers', usersConectados);
 
 server.listen(5175, () => {
