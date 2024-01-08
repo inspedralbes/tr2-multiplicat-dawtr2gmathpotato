afc00fea05611218f20b7600edf1507a8db22443
Eric Gómez Vilà <a20erigomvil@inspedralbes.cat>
2024-01-08 10:15:53 +0100

    SomeOfTheBugsFixed

--- tr2-MathPotato-Front/src/socket.js
+++ tr2-MathPotato-Front/src/socket.js
@@ -109,7 +109,7 @@ function getCurrentUser(users) {
         store.setTimer(dataPartida.timer);
         // store.setGuestInfo({ lives: 0});
         socket.emit('join', {"username":dataPartida.username, "image":dataPartida.image});
-        socket.emit('eliminarPartida', dataPartida.roomName);
+        
     });
 
     socket.on("loginError", (error) => {
